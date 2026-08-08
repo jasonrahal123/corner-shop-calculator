5c651b0a3245c7489ae196c39bd2f6ca1689e9d4
Jason Rahal <jason.rahal4@gmail.com>
2026-08-08 16:32:32 +1000

    Set up GitHub with PyCharm using terminal

---
+++ Corner Shop Calculator/task/calculator.py
@@ -0,0 +1,9 @@
+# Write your code here
+print("Prices: ")
+print("Bubblegum: $0.2")
+print("Toffee: $0.2")
+print("Ice cream: $5")
+print("Milk chocolate: $4")
+print("Doughnut: $2.5")
+print("Pancake: $3.2")
+
